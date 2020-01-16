183c483e7580cb6da536a12b97c87d4b7a7620f9
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-01-15 22:39:48 -0500

    More implementation, moved functions inside the installation procedure

--- 2_second/2-87-PolyZero.scm
+++ 2_second/2-87-PolyZero.scm
@@ -1,6 +1,7 @@
 (require racket/include)
 
 (require racket/trace)
+
 ;; 2020-01-15 20:31 :: zenAndroid
 ;; I am being intentionnally verbose.
 ;; The code honestly seems self-documenting, however maybe i'll change opinion
@@ -9,66 +10,6 @@
 
 ;; Either way, the best way *I* understood this was by taking apen and paper
 ;; and going through the process manually.
-
-;; Adding two polynomials
-
-(define (add-polys P1 P2)
-  (if (same-variable? P1 P2)
-      (make-poly 
-        (variable P1)
-        (add-terms 
-          (term-list P1)
-          (term-list P2))))
-      (error "Polynomials not in same variable " (list P1 P2)))
-
-;; Multiplying two polynomials
-
-(define (mul-polys P1 P2)
-  (if (same-variable? P1 P2)
-      (make-poly 
-        (variable P1)
-        (mul-terms 
-          (term-list P1)
-          (term-list P2))))
-      (error "Polynomials not in same variable " (list P1 P2)))
-
-;; Adding two polynomial term lists
-
-(define (add-terms term-list1 term-list2)
-  (cond ((empty-term-list? term-list1) term-list2)
-        ((empty-term-list? term-list2) term-list1)
-        (else 
-          (let ((t1 (first-term term-list1))
-                (t2 (first-term term-list2)))
-            (cond ((> (order t1) (order t2)) 
-                   (append-terms t1
-                                 (add-terms (rest-of-terms term-list1) term-list2)))
-                  ((< (order t1) (order t2))
-                   (append-terms t2
-                                 (add-terms term-list1 (rest-of-terms term-list2))))
-                  (else
-                    (append-terms (make-term (order t1) (add (coeff t1) (coeff t2)))
-                                  (add-terms 
-                                    (rest-of-terms term-list1)
-                                    (rest-of-terms term-list2)))))))))
-
-
-;; Multiplying two polynomial term lists.
-
-(define (mul-terms term-list1 term-list2)
-  (define (mul-by-all-terms term term-list)
-    (if (empty-term-list? term-list)
-        the-empty-term-list
-        (append-terms
-          (make-term 
-            (+ (order term) (order (first-term term-list))) 
-            (mul (coeff t1) (coeff (first-term term-list))))
-          (mul-terms term (rest-of-terms term-list)))))
-  (cond ((empty-term-list? term-list1) the-empty-term-list)
-        (else
-          (add-terms 
-            (mul-by-all-terms (first-term term-list1) term-list2)
-            (mul-terms (rest-of-terms term-list1) term-list2)))))
 
 ;; Let's see what I need now:
 ;; order, coeff        ==> for the term
@@ -84,44 +25,107 @@
 
 (define (install-polynomial-package op-table)
 
+  ;; Adding two polynomials
+
+  (define (add-polys P1 P2)
+    (if (same-variable? P1 P2)
+        (make-poly
+          (variable P1)
+          (add-terms
+            (term-list P1)
+            (term-list P2))))
+        (error "Polynomials not in same variable " (list P1 P2)))
+ 
+  ;; Multiplying two polynomials
+ 
+  (define (mul-polys P1 P2)
+    (if (same-variable? P1 P2)
+        (make-poly
+          (variable P1)
+          (mul-terms
+            (term-list P1)
+            (term-list P2))))
+        (error "Polynomials not in same variable " (list P1 P2)))
+ 
+  ;; Adding two polynomial term lists
+ 
+  (define (add-terms term-list1 term-list2)
+    (cond ((empty-term-list? term-list1) term-list2)
+          ((empty-term-list? term-list2) term-list1)
+          (else
+            (let ((t1 (first-term term-list1))
+                  (t2 (first-term term-list2)))
+              (cond ((> (order t1) (order t2))
+                     (append-terms t1
+                                   (add-terms (rest-of-terms term-list1) term-list2)))
+                    ((< (order t1) (order t2))
+                     (append-terms t2
+                                   (add-terms term-list1 (rest-of-terms term-list2))))
+                    (else
+                      (append-terms (make-term (order t1) (add (coeff t1) (coeff t2)))
+                                    (add-terms
+                                      (rest-of-terms term-list1)
+                                      (rest-of-terms term-list2)))))))))
+ 
+ 
+  ;; Multiplying two polynomial term lists.
+ 
+  (define (mul-terms term-list1 term-list2)
+    (define (mul-by-all-terms term term-list)
+      (if (empty-term-list? term-list)
+          the-empty-term-list
+          (append-terms
+            (make-term
+              (+ (order term) (order (first-term term-list)))
+              (mul (coeff t1) (coeff (first-term term-list))))
+            (mul-terms term (rest-of-terms term-list)))))
+    (cond ((empty-term-list? term-list1) the-empty-term-list)
+          (else
+            (add-terms
+              (mul-by-all-terms (first-term term-list1) term-list2)
+              (mul-terms (rest-of-terms term-list1) term-list2)))))
+
   (define (tag x) (attach-tag 'polynomial))
 
   (define (make-poly variable term-list)
     (cons variable term-list))
 
-  (define (variable poly)
-    (car poly))
+  (define (variable poly) (car poly))
 
-  (define (term-list poly)
-    (cdr poly))
+  (define (term-list poly) (cdr poly))
 
-  (define (first-term poly)
-    (car (term-list poly)))
+  (define (first-term term-list) (car term-list))
 
-  (define (variable? var)
-    (symbol? var))
+  (define (rest-of-terms term-list) (cdr term-list))
+
+  (define (variable? var) (symbol? var))
 
   (define (same-variable? v1 v2)
     (and (variable? v1)
          (variable? v2)
          (eq? v1 v2)))
 
-  (define empty-term-list? (`ToDo))
+  (define empty-term-list? (null? the-empty-term-list))
 
-  (define the-empty-term-list (`ToDo))
+  (define the-empty-term-list '())
 
-  (define make-term (`ToDo))
+  (define (make-term order coeff) (list order coeff))
 
-  (define order (`ToDo))
+  (define (order term) (car term))
 
-  (define coeff (`ToDo))
+  (define (coeff term) (cadr term))
+
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
 
   ;; ---------------------------------
 
 
-  (define first-op 
-    (put op-table 'make 'polynomial 
-         (lambda(polyvar poly-term-list) 
+  (define first-op
+    (put op-table 'make 'polynomial
+         (lambda(polyvar poly-term-list)
            (tag (make-poly polyvar poly-term-list)))))
 
   (define second-op
@@ -133,3 +137,9 @@
          (lambda(pol1 pol2) (tag (mul-polys pol1 pol2)))))
 
   third-op)
+
+
+;; Exposing the functions to make polynomials
+
+(define (make-poly variable term-list)
+  ((get MAIN-TABLE 'poly 'make) variable term-list))
