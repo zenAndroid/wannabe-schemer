af718de249bdec8c3a067eb23ccfbcfd7bf4edad
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-01-16 12:47:00 -0500

    Implement polynomial package, improve integer handling.

--- 2_second/2-80-basicDefinitions.scm
+++ 2_second/2-80-basicDefinitions.scm
@@ -1,3 +1,5 @@
+(require racket/trace)
+
 ; Functions to handle a table of triplets
 
 
@@ -13,17 +15,19 @@
 ; Functions to handle tagged data
 
 (define (attach-tag type-tag contents)
-  (cons type-tag contents))
+  (if (number? contents)
+      contents
+      (cons type-tag contents)))
 
 (define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum: TYPE-TAG" datum)))
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (else (error "Bad tagged datum: TYPE-TAG" datum))))
 
 (define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum: CONTENTS" datum)))
+  (cond ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+        (else (error "Bad tagged datum: CONTENTS" datum))))
 
 
 ; Functions to handle generic functionality
--- 2_second/2-87-PolyZero.scm
+++ 2_second/2-87-PolyZero.scm
@@ -2,6 +2,8 @@
 
 (require racket/trace)
 
+(include "2-81GenericApply-generic.scm")
+
 ;; 2020-01-15 20:31 :: zenAndroid
 ;; I am being intentionnally verbose.
 ;; The code honestly seems self-documenting, however maybe i'll change opinion
@@ -25,27 +27,61 @@
 
 (define (install-polynomial-package op-table)
 
+  (define (tag x) (attach-tag 'polynomial x))
+
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+
+  (define (variable poly) (car poly))
+
+  (define (term-list poly) (cdr poly))
+
+  (define (first-term term-list) (car term-list))
+
+  (define (rest-of-terms term-list) (cdr term-list))
+
+  (define (variable? var) (symbol? var))
+
+  (define (same-variable? v1 v2)
+    (and (variable? v1)
+         (variable? v2)
+         (eq? v1 v2)))
+
+  (define (empty-term-list? term-list) (null? term-list))
+
+  (define the-empty-term-list '())
+
+  (define (make-term order coeff) (list order coeff))
+
+  (define (order term) (car term))
+
+  (trace-define (coeff term) (cadr term))
+
+  (define (append-terms term term-list)
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
   ;; Adding two polynomials
 
   (define (add-polys P1 P2)
-    (if (same-variable? P1 P2)
+    (if (same-variable? (variable P1) (variable P2))
         (make-poly
           (variable P1)
           (add-terms
             (term-list P1)
-            (term-list P2))))
-        (error "Polynomials not in same variable " (list P1 P2)))
+            (term-list P2)))
+        (error "Polynomials not in same variable " (list P1 P2))))
  
   ;; Multiplying two polynomials
  
   (define (mul-polys P1 P2)
-    (if (same-variable? P1 P2)
+    (if (same-variable? (variable P1) (variable P2))
         (make-poly
           (variable P1)
           (mul-terms
             (term-list P1)
-            (term-list P2))))
-        (error "Polynomials not in same variable " (list P1 P2)))
+            (term-list P2)))
+        (error "Polynomials not in same variable " (list P1 P2))))
  
   ;; Adding two polynomial term lists
  
@@ -71,54 +107,23 @@
   ;; Multiplying two polynomial term lists.
  
   (define (mul-terms term-list1 term-list2)
-    (define (mul-by-all-terms term term-list)
-      (if (empty-term-list? term-list)
-          the-empty-term-list
-          (append-terms
-            (make-term
-              (+ (order term) (order (first-term term-list)))
-              (mul (coeff t1) (coeff (first-term term-list))))
-            (mul-terms term (rest-of-terms term-list)))))
-    (cond ((empty-term-list? term-list1) the-empty-term-list)
+    (cond ((empty-term-list? term-list1) 
+           ('()))
           (else
             (add-terms
               (mul-by-all-terms (first-term term-list1) term-list2)
               (mul-terms (rest-of-terms term-list1) term-list2)))))
 
-  (define (tag x) (attach-tag 'polynomial))
 
-  (define (make-poly variable term-list)
-    (cons variable term-list))
+  (define (mul-by-all-terms term term-list)
+    (if (empty-term-list? term-list)
+        ('())
+        (append-terms
+          (make-term
+            (+ (order term) (order (first-term term-list)))
+            (mul (coeff t1) (coeff (first-term term-list))))
+          (mul-terms term (rest-of-terms term-list)))))
 
-  (define (variable poly) (car poly))
-
-  (define (term-list poly) (cdr poly))
-
-  (define (first-term term-list) (car term-list))
-
-  (define (rest-of-terms term-list) (cdr term-list))
-
-  (define (variable? var) (symbol? var))
-
-  (define (same-variable? v1 v2)
-    (and (variable? v1)
-         (variable? v2)
-         (eq? v1 v2)))
-
-  (define empty-term-list? (null? the-empty-term-list))
-
-  (define the-empty-term-list '())
-
-  (define (make-term order coeff) (list order coeff))
-
-  (define (order term) (car term))
-
-  (define (coeff term) (cadr term))
-
-  (define (adjoin-term term term-list)
-    (if (=zero? (coeff term))
-        term-list
-        (cons term term-list)))
 
   ;; ---------------------------------
 
@@ -142,4 +147,40 @@
 ;; Exposing the functions to make polynomials
 
 (define (make-poly variable term-list)
-  ((get MAIN-TABLE 'poly 'make) variable term-list))
+  ((get MAIN-TABLE 'make 'polynomial) variable term-list))
+
+(define MAIN-TABLE (install-polynomial-package MAIN-TABLE))
+
+; (map displayln MAIN-TABLE)
+
+(define poly-example
+  (make-poly 'x '((7 2) (5 -4) (2 3) (1 1) (0 7))))
+
+(define p
+  (make-poly 'x '((4 2) (2 4) (0 1))))
+
+; (map displayln (list p poly-example))
+; 
+; (displayln (add p poly-example))
+
+(define complex (make-complex-from-real-imag 8 7))
+(define rqt (make-rat 7 8))
+
+(define t1 (list 3 complex))
+(define t2 (list 1 rqt))
+(define term-lost (list t1 t2))
+(define zero-list (list
+                    (list 3 (make-rat 0 2))
+                    (list 2 0)
+                    (list 1 (make-complex-from-real-imag 0 0))))
+
+(define zero-pol (make-poly 'y zero-list))
+
+(define pol (make-poly 'x term-lost))
+;(make-poly 'x (list (list 3 5)
+                    ;(list 2 (make-rat 3 4))
+                    ;(list 1 (make-complex-from-real-imag 5 0))))
+(displayln pol)
+
+(displayln (add pol poly-example))
+
