bb71cff1e6fe7f4a03d9847232504f39404cd48b
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-01-30 22:56:07 -0500

    This script is nifty.
    Got it from https://github.com/zv/SICP-guile, credit to him.
    
    Uses GraphViz to illustrate list structures.

---
+++ 3_third/GraphThoseLists.scm
@@ -0,0 +1,49 @@
+(define (list->graphviz lst)
+  """Convert a list into a set of Graphviz instructions"""
+  (define number 0)
+  (define result "")
+  (define ordinals '())
+  (define (result-append! str)
+    (set! result (string-append result str)))
+
+  (define* (nodename n #:optional cell)
+    (format #f "cons~a~a" n (if cell (string-append ":" cell) "")))
+
+  (define* (build-connector from to #:optional from-cell)
+    (format #f "\t~a -> ~a;~%" (nodename from from-cell) (nodename to)))
+
+  (define (build-shape elt)
+    (define (build-label cell)
+      (cond ((null? cell) "&#x2205;") ; null character
+            ((pair? cell) "&#x2022;") ; bullet dot character
+            (else (format #f "~a" cell))))
+    (set! number (+ number 1))
+
+    (format #f "\t~a [shape=record,label=\"<car> ~a | <cdr> ~a\"];~%"
+            (nodename number)
+            (build-label (car elt))
+            (build-label (cdr elt))))
+
+  (define* (search xs #:optional from-id from-cell)
+    (let ((existing (assq xs ordinals)))
+      (if (pair? existing) ;; handle lists with cycles
+          ;; we've already built a node for this entry, just make a connector
+          (result-append! (build-connector from-id (cdr existing) from-cell))
+          (begin
+            (result-append! (build-shape xs))
+            (set! ordinals (assq-set! ordinals xs number))
+            (let ((parent-id number))
+              ;; make a X->Y connector
+              (if (number? from-id)
+                  (result-append! (build-connector from-id parent-id from-cell)))
+              ;; recurse
+              (if (pair? (car xs)) (search (car xs) parent-id "car"))
+              (if (pair? (cdr xs)) (search (cdr xs) parent-id "cdr")))))))
+
+  (search lst)
+  (string-append "digraph G {\n" result "}\n"))
+
+
+(define the-list (list 1 2 3))
+
+(display (list->graphviz the-list))
