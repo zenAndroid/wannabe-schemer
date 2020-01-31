b132248bb6c827904357a1d3c51f02a67f7a6690
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-01-30 22:52:52 -0500

    Exercise 3.18 seems to have been finished.
    I am sure one can come up with perverted cases where it fails,
    but this solution seems reasonable to me.
    Plus, i spent far too much time on this.

--- 3_third/18-CycleDetection.scm
+++ 3_third/18-CycleDetection.scm
@@ -1,25 +1,27 @@
 (define the-list (list 1 2 3))
 
-(display the-list)
+(set-cdr! (cddr the-list) the-list)
+
+(define (has-cycles? arg-list)
+  (define encountered '())
+  (define (traverse list-item)
+    (if (or (not (pair? list-item)) (null? list-item))
+      #f
+      (let ((element-encountered?
+              (memq list-item encountered)))
+        (cond ((not element-encountered?)
+               (begin (set! encountered (cons list-item encountered))
+                      (traverse (cdr list-item))))
+              (element-encountered? #t)))))
+  (traverse arg-list))
+
 (newline)
 
-(set-cdr! (cddr the-list)
-          (cdr the-list))
+(define t1 (list 'a 'b))
+(define t2 (list t1 t1))
 
-(display the-list)
+(display (has-cycles? t2))
 (newline)
+(display (has-cycles? the-list))
 
-(define (has-cycles arg-list)
-  (let ((encountered '()))
-    (define (traverse list-item)
-      (if (null? list-item)
-        #f
-        (let ((element-encountered?
-                (memq list-item encountered)))
-          (cond ((not element-encountered?)
-                 (begin (set! encountered (cons list-item encountered))
-                        (display encountered)
-                        (newline)
-                        (traverse (cdr list-item))))
-                ((element-encountered?) #t)))))
-    (traverse arg-list)))
+;Seems to work ...
