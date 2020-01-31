84cc8233945298bf79910630813e4ce272a69af0
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-01-31 10:58:41 -0500

    I will be satisfied with this, even thoug it is not perfect.

--- 3_third/18-CycleDetection.scm
+++ 3_third/18-CycleDetection.scm
@@ -1,7 +1,3 @@
-(define the-list (list 1 2 3))
-
-(set-cdr! (cddr the-list) the-list)
-
 (define (has-cycles? arg-list)
   (define encountered '())
   (define (traverse list-item)
@@ -13,7 +9,7 @@
                (begin (set! encountered (cons list-item encountered))
                       (or (traverse (car list-item))
                           (traverse (cdr list-item)))))
-              (element-encountered? (has-cycles? list-item))))))
+              (element-encountered? list-item)))))
   (traverse arg-list))
 
 (define (cycle-haver arg-list)
@@ -26,10 +22,21 @@
 (define t1 (list 'a 'b))
 (define t2 (list t1 t1))
 (define he-list (list 1 2 3))
+(define the-list (list 1 2 3))
 (set-car! (cddr he-list) (cdr he-list))
-
+(set-cdr! (cddr the-list) the-list)
 (cycle-haver t2)
 (cycle-haver the-list)
 (cycle-haver he-list)
-
+(cycle-haver (list (list 2 3 4) (list 3 42 5) (cons 3 4)))
 ;Seems to work ...
+
+; Result of execution :
+
+
+;;; note: source file /home/zenandroid/gitRepos/scheming/3_third/18-CycleDetection.scm
+;;; newer than compiled /home/zenandroid/.cache/guile/ccache/2.2-LE-8-3.A/home/zenandroid/gitRepos/scheming/3_third/18-CycleDetection.scm.go
+; The list ((a b) (a b)) DOES have a cycle
+; The list (1 2 3 . #-2#) DOES have a cycle
+; The list (1 2 #-1#) DOES have a cycle
+; The list ((2 3 4) (3 42 5) (3 . 4)) DOES NOT have a cycle
