4162623b37a3a30135359a47ee04bf420e636af3
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-02-15 11:28:24 -0500

    Nice:

---
+++ 3_third/43-SerialzedExchanges.scm
@@ -0,0 +1,12 @@
+; I don't understand the question, if each individuak exchange leaves the state
+; of the accounts consistent, then it seems trivial that a sreies of account
+; exchanges would do so as well ??
+; 
+; As for the second part of the question, I drerw an example of a time diagram in
+; my textbook, cant find an easy way to reproduce it here, so /shrug.
+; 
+; As for the third part of the question, I think even the unserialized version of
+; the exchange procedure will leave the final state of the account such that
+; their sum wont change because at least the differences are calculated in an
+; atomic, non-interleaved way, so I figure that has something to do with it, ...
+
