01ad3d7784b64b0a26821897c168791a85fd6114
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-02-25 20:31:58 -0500

    Here we go.
    First exercise, ... well you know, wasn't that hard but still

---
+++ 4_fourth/01-EvalOrder.scm
@@ -0,0 +1,11 @@
+; 2020-02-25 20:10 :: zenAndroid ::  A new chapter, a new opputunity, here we go i've been looking forward for this ...
+
+
+; Also, I don't think I can test this but yea I pretty much skipped doing this exercise because just by reading 
+; the question of the exercise I immediately thought of that Chapter 3 exercise
+; that talked abour the order of evaluation and how it can lead to different
+; results in performing set! operations
+
+; Ive checked the solution (from Eli) and as I supected its a matter of let
+; bindings (Eli used the let* form. I dislike that because it's a bit too
+; procedural for my taste)
