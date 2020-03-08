69cdbc320d297778bfaa4be473bad1c729be3995
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-03-08 16:19:22 -0400

    eval -> zeval, output

--- 4_fourth/27-HeckingSideEffects.scm
+++ 4_fourth/27-HeckingSideEffects.scm
@@ -3,7 +3,7 @@
 
 (load "00-The-Actual-Meta-Eval.scm")
 
-(define (eval exp env)
+(define (zeval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
         ((quoted? exp) (text-of-quotation exp))
@@ -16,7 +16,7 @@
                          env))
         ((begin? exp) 
          (eval-sequence (begin-actions exp) env))
-        ((cond? exp) (eval (cond->if exp) env))
+        ((cond? exp) (zeval (cond->if exp) env))
         ((application? exp)             ; clause from book
          (apply (actual-value (operator exp) env)
                 (operands exp)
@@ -25,7 +25,7 @@
          (error "Unknown expression type -- EVAL" exp))))
 
 (define (actual-value exp env)
-  (force-it (eval exp env)))
+  (force-it (zeval exp env)))
 
 (define (apply procedure arguments env)
   (cond ((primitive-procedure? procedure)
@@ -59,8 +59,8 @@
 
 (define (eval-if exp env)
   (if (true? (actual-value (if-predicate exp) env))
-      (eval (if-consequent exp) env)
-      (eval (if-alternative exp) env)))
+      (zeval (if-consequent exp) env)
+      (zeval (if-alternative exp) env)))
 
 (define input-prompt ";;; L-Eval input:")
 (define output-prompt ";;; L-Eval value:")
@@ -126,3 +126,56 @@
         ))
 
 'LAZY-EVALUATOR-LOADED
+
+
+
+;;; ;;; ;;; [zenandroid ~/gitRepos/scheming/4_fourth]$ guile -l 27-HeckingSideEffects.scm
+;;; ;;; ;;; GNU Guile 2.2.6
+;;; ;;; ;;; Copyright (C) 1995-2019 Free Software Foundation, Inc.
+;;; ;;; ;;; 
+;;; ;;; ;;; Guile comes with ABSOLUTELY NO WARRANTY; for details type `,show w'.
+;;; ;;; ;;; This program is free software, and you are welcome to redistribute it
+;;; ;;; ;;; under certain conditions; type `,show c' for details.
+;;; ;;; ;;; 
+;;; ;;; ;;; Enter `,help' for help.
+;;; ;;; ;;; scheme@(guile-user)> (driver-loop)
+;;; ;;; ;;; 
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; (define cnt 0)
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; ok
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; (define (id x) (set! cnt (+ cnt 1)) x)
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; ok
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; (define w (id (id 10)))
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; ok
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; cnt
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; 1
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; w
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; 10
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; cnt
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval value:
+;;; ;;; ;;; 2
+;;; ;;; ;;; 
+;;; ;;; ;;; ;;; L-Eval input:
+;;; ;;; ;;; 
