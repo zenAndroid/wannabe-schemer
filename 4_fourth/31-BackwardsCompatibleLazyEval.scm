190dae76a39390f34afb8f6faedc48c15655be6e
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-03-14 17:08:15 -0400

    Copy paste from the meta circular evaluator.
    To help see the diff.

--- 4_fourth/31-BackwardsCompatibleLazyEval.scm
+++ 4_fourth/31-BackwardsCompatibleLazyEval.scm
@@ -125,3 +125,17 @@
         (else
           (cons (zeval (car exps) env)
                 (list-of-values (cdr formal-parameters) (cdr exps) env)))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             arguments
+             (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
