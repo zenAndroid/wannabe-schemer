a7660dacfc19d1e297b518be1b3b4fd41864ea22
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-03-18 21:50:37 -0400

    Add question

--- 4_fourth/38-LogicPuzzle.scm
+++ 4_fourth/38-LogicPuzzle.scm
@@ -1,5 +1,10 @@
 ; 2020-03-18 21:44 :: zenAndroid :: Well, that's rather inefficient ...
 
+; Exercise 4.38: Modify the multiple-dwelling procedure to omit the requirement
+; that Smith and Fletcher do not live on adjacent floors. How many solutions
+; are there to this modified puzzle?
+
+
 ; Just remove the corresponding require line?
 
 
