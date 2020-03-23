1ff91b211e6a71f6436dded81d0cf24b208b2597
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-03-23 15:01:18 -0400

    Add exercise 4-42, didnot run it yet

---
+++ 4_fourth/42-LyingSchoolGirls.scm
@@ -0,0 +1,33 @@
+; 2020-03-23 14:04 :: zenAndroid :: Here we go ...
+
+(load "00-AmbEval.scm") 
+
+(driver-loop)
+
+(define (require p)
+  (if (not p)
+    (amb)))
+(define (distinct? items)
+  (cond ((null? items) true)
+        ((null? (cdr items)) true)
+        ((member (car items) (cdr items)) false)
+        (else (distinct? (cdr items)))))
+
+(define (lying-schoolgirls)
+  (let ((betty (amb 1 2 3 4 5))
+        (ethel (amb 1 2 3 4 5))
+        (joan (amb 1 2 3 4 5))
+        (kitty (amb 1 2 3 4 5))
+        (mary (amb 1 2 3 4 5)))
+    (amb (require (= betty 3)) (require (= kitty 2)))
+    (amb (require (= ethel 1)) (require (= joan 2)))
+    (amb (require (= joan 3)) (require (= ethel 5)))
+    (amb (require (= kitty 2)) (require (= mary 4)))
+    (amb (require (= mary 4)) (require (= betty 1)))
+    (list ('betty betty)
+          ('ethel ethel)
+          ('joan joan)
+          ('kitty kitty)
+          ('mary mary))))
+
+(lying-schoolgirls)
