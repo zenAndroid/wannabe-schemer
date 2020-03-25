1b0240371920ed2064972120a3b053ed8b580366
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2020-03-24 23:27:07 -0400

    Thank god for Vim Macros, helped me pretty print that mess

--- 4_fourth/45-NaturalLanguageParsing.scm
+++ 4_fourth/45-NaturalLanguageParsing.scm
@@ -118,3 +118,153 @@
 
 
 ;; Yey !
+
+
+; Exercise 4.45: With the grammar given above, the following sentence can be
+; parsed in five different ways: “The professor lectures to the student in the ; class with the cat.” Give the five parses and explain the differences in
+; shades of meaning among them.
+
+(parse '(the professor lectures to the student in the class with the cat))
+
+;;; Amb-Eval input:
+(parse '(the professor lectures to the student in the class with the cat))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+(sentence 
+  (simple-noun-phrase 
+    (article the) 
+    (noun professor)) 
+  (verb-phrase 
+    (verb-phrase 
+      (verb-phrase 
+        (verb lectures) 
+        (prep-phrase 
+          (prep to) 
+          (simple-noun-phrase 
+            (article the) 
+            (noun student)))) 
+      (prep-phrase 
+        (prep in) 
+        (simple-noun-phrase 
+          (article the) 
+          (noun class)))) 
+    (prep-phrase 
+      (prep with) 
+      (simple-noun-phrase 
+        (article the) 
+        (noun cat)))))
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval value:
+(sentence 
+  (simple-noun-phrase 
+    (article the) 
+    (noun professor)) 
+  (verb-phrase 
+    (verb-phrase 
+      (verb lectures) 
+      (prep-phrase 
+        (prep to) 
+        (simple-noun-phrase 
+          (article the) 
+          (noun student)))) 
+    (prep-phrase 
+      (prep in) 
+      (noun-phrase 
+        (simple-noun-phrase 
+          (article the) 
+          (noun class)) 
+        (prep-phrase 
+          (prep with) 
+          (simple-noun-phrase 
+            (article the) 
+            (noun cat)))))))
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval value:
+(sentence 
+  (simple-noun-phrase 
+            (article the) 
+            (noun professor)) 
+          (verb-phrase 
+            (verb-phrase 
+              (verb lectures) 
+              (prep-phrase 
+                (prep to) 
+                (noun-phrase 
+                  (simple-noun-phrase 
+                    (article the) 
+                    (noun student)) 
+                  (prep-phrase 
+                    (prep in) 
+                    (simple-noun-phrase 
+                      (article the) 
+                      (noun class)))))) 
+            (prep-phrase 
+              (prep with) 
+              (simple-noun-phrase 
+                (article the) 
+                (noun cat)))))
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval value:
+(sentence (simple-noun-phrase 
+            (article the) 
+            (noun professor)) 
+          (verb-phrase 
+            (verb lectures) 
+            (prep-phrase 
+              (prep to) 
+              (noun-phrase 
+                (noun-phrase 
+                  (simple-noun-phrase 
+                    (article the) 
+                    (noun student)) 
+                  (prep-phrase 
+                    (prep in) 
+                    (simple-noun-phrase 
+                      (article the) 
+                      (noun class)))) 
+                (prep-phrase 
+                  (prep with) 
+                  (simple-noun-phrase 
+                    (article the) 
+                    (noun cat)))))))
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval value:
+(sentence 
+  (simple-noun-phrase 
+    (article the) 
+    (noun professor)) 
+  (verb-phrase 
+    (verb lectures) 
+    (prep-phrase 
+      (prep to) 
+      (noun-phrase 
+        (simple-noun-phrase 
+          (article the) 
+          (noun student)) 
+        (prep-phrase 
+          (prep in) 
+          (noun-phrase 
+            (simple-noun-phrase 
+              (article the) 
+              (noun class)) 
+            (prep-phrase 
+              (prep with) 
+              (simple-noun-phrase 
+                (article the) 
+                (noun cat)))))))))
+;;; Amb-Eval input:
+try-again
+
+;;; There are no more values of
+(parse (quote (the professor lectures to the student in the class with the cat)))
+;;; Amb-Eval input:
+
