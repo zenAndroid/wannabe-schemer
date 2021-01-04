802a81fc2036c5e611b18ebaf29a8cc02087e604
zenAndroid <zena02@uqo.ca>
2021-01-04 18:14:30 -0500

    Ehm this has took me way too long, but ill try to finish this prototype at least

--- 5_fifth/ch5-regsim.scm
+++ 5_fifth/ch5-regsim.scm
@@ -429,3 +429,48 @@
       (assign val (const 1))   
       (goto (reg continue))
       done)))
+
+(define fib-machine 
+  (make-machine ;register-names ops controller-text 
+    '(n val continue) 
+    (list (list '< <) (list '- -) (list '+ +)) 
+    '(  ; from ch5.scm 
+        (assign continue (label fib-done)) 
+        fib-loop 
+        (test (op <) (reg n) (const 2)) 
+        (branch (label immediate-answer)) 
+        ;; set up to compute Fib(n-1) 
+        (save continue) 
+        (assign continue (label afterfib-n-1)) 
+        (save n)                           ; save old value of n 
+        (assign n (op -) (reg n) (const 1)); clobber n to n-1 
+        (goto (label fib-loop))            ; perform recursive call 
+        afterfib-n-1                         ; upon return, val contains Fib(n-1) 
+        (restore n) 
+        (restore continue) 
+        ;; set up to compute Fib(n-2) 
+        (assign n (op -) (reg n) (const 2)) 
+        (save continue) 
+        (assign continue (label afterfib-n-2)) 
+        (save val)                         ; save Fib(n-1) 
+        (goto (label fib-loop)) 
+        afterfib-n-2                         ; upon return, val contains Fib(n-2) 
+        (assign n (reg val))               ; n now contains Fib(n-2) 
+        (restore val)                      ; val now contains Fib(n-1) 
+        (restore continue) 
+        (assign val                        ; Fib(n-1)+Fib(n-2) 
+                (op +) (reg val) (reg n))  
+        (goto (reg continue))              ; return to caller, answer is in val 
+        immediate-answer 
+        (assign val (reg n))               ; base case: Fib(n)=n 
+        (goto (reg continue)) 
+        fib-done)))
+
+;; scheme@(guile-user)> (set-register-contents! fib-machine 'n 14)
+;; $1 = done
+;; scheme@(guile-user)> (start fib-machine )
+;; $2 = done
+;; scheme@(guile-user)> (get-register-contents fib-machine 'val)
+;; $3 = 377
+
+;; So that's cool
