6d2a9eb63d46ec1711ec3097e5b48a4e34993ed2
zenAndroid <zena02@uqo.ca>
2021-01-07 16:43:31 -0500

    Trying to complete this piece of shit

--- 5_fifth/scratchPad.scm
+++ 5_fifth/scratchPad.scm
@@ -1,136 +1,103 @@
-(use-modules (ice-9 pretty-print))
+(use-modules (ice-9 pretty-print)) ; Pretty printing
+(use-modules (srfi srfi-1)) ; Remove duplicate
+;{{{ Factorial machine
+(define smh-im-hecking-tired 
+  '((assign continue (label done))
+    loop
+    (test (op =) (reg n) (const 0))
+    (branch (label base-case))
+    (save continue)
+    (assign continue (label after))
+    (assign n (op -) (reg n) (const 1))
+    (assign n (reg z))
+    (goto (label loop))
+    after
+    (restore continue)
+    (assign val (op *) (reg val) (reg b))
+    (goto (reg continue))
+    base-case
+    (assign val (const 1))   
+    (goto (reg continue))
+    done));}}}
+;{{{ Fibonacci machine
+(define oh-man 
+  '(  ; from ch5.scm 
+      (assign continue (label fib-done)) 
+      fib-loop 
+      (test (op <) (reg n) (const 2)) 
+      (branch (label immediate-answer)) 
+      ;; set up to compute Fib(n-1) 
+      (save continue) 
+      (assign continue (label afterfib-n-1)) 
+      (save n)                           ; save old value of n 
+      (assign n (op -) (reg n) (const 1)); clobber n to n-1 
+      (goto (label fib-loop))            ; perform recursive call 
+      afterfib-n-1                         ; upon return, val contains Fib(n-1) 
+      (restore n) 
+      (restore continue) 
+      ;; set up to compute Fib(n-2) 
+      (assign n (op -) (reg n) (const 2)) 
+      (save continue) 
+      (assign continue (label afterfib-n-2)) 
+      (save val)                         ; save Fib(n-1) 
+      (goto (label fib-loop)) 
+      afterfib-n-2                         ; upon return, val contains Fib(n-2) 
+      (assign n (reg val))               ; n now contains Fib(n-2) 
+      (restore val)                      ; val now contains Fib(n-1) 
+      (restore continue) 
+      (assign val                        ; Fib(n-1)+Fib(n-2) 
+              (op +) (reg val) (reg n))  
+      (goto (reg continue))              ; return to caller, answer is in val 
+      immediate-answer 
+      (assign val (reg n))               ; base case: Fib(n)=n 
+      (goto (reg continue)) 
+      fib-done));}}}
 
-(define smh-im-hecking-tired '(
-                               (assign continue (label done))
-                               loop
-                               (test (op =) (reg n) (const 0))
-                               (branch (label base-case))
-                               (save continue)
-                               (assign continue (label after))
-                               (assign n (op -) (reg n) (const 1))
-                               (goto (label loop))
-                               after
-                               (restore continue)
-                               (assign val (op *) (reg val) (reg b))
-                               (goto (reg continue))
-                               base-case
-                               (assign val (const 1))   
-                               (goto (reg continue))
-                               done))
-
-
-(define oh-man '(  ; from ch5.scm 
-                   (assign continue (label fib-done)) 
-                   fib-loop 
-                   (test (op <) (reg n) (const 2)) 
-                   (branch (label immediate-answer)) 
-                   ;; set up to compute Fib(n-1) 
-                   (save continue) 
-                   (assign continue (label afterfib-n-1)) 
-                   (save n)                           ; save old value of n 
-                   (assign n (op -) (reg n) (const 1)); clobber n to n-1 
-                   (goto (label fib-loop))            ; perform recursive call 
-                   afterfib-n-1                         ; upon return, val contains Fib(n-1) 
-                   (restore n) 
-                   (restore continue) 
-                   ;; set up to compute Fib(n-2) 
-                   (assign n (op -) (reg n) (const 2)) 
-                   (save continue) 
-                   (assign continue (label afterfib-n-2)) 
-                   (save val)                         ; save Fib(n-1) 
-                   (goto (label fib-loop)) 
-                   afterfib-n-2                         ; upon return, val contains Fib(n-2) 
-                   (assign n (reg val))               ; n now contains Fib(n-2) 
-                   (restore val)                      ; val now contains Fib(n-1) 
-                   (restore continue) 
-                   (assign val                        ; Fib(n-1)+Fib(n-2) 
-                           (op +) (reg val) (reg n))  
-                   (goto (reg continue))              ; return to caller, answer is in val 
-                   immediate-answer 
-                   (assign val (reg n))               ; base case: Fib(n)=n 
-                   (goto (reg continue)) 
-                   fib-done))
-
-(define (register-exp? exp) (tagged-list? exp 'reg))
-
-(define (register-exp-reg exp) (cadr exp))
-
-(define (constant-exp? exp) (tagged-list? exp 'const))
-
-(define (constant-exp-value exp) (cadr exp))
-
-(define (label-exp? exp) (tagged-list? exp 'label))
-
-(define (label-exp-label exp) (cadr exp))
-
-(define (tagged-list? exp tag)
-  (if (pair? exp)
-      (eq? (car exp) tag)
-      false))
-
-(define (stack-inst-reg-name stack-instruction)
-  (cadr stack-instruction))
-
-(define (goto-dest goto-instruction)
-  (cadr goto-instruction))
-
-(define (goto-reg-extract goto-dest)
-  (cadr goto-dest))
-
-(define (assign-reg-name assign-instruction)
-  (cadr assign-instruction))
-
-(define (assign-value-exp assign-instruction)
-  (cddr assign-instruction))
-
-
-(define (inst-scanner insts)
-
-  (define sorted-instructions '())
-  (define entry-points '())
-  (define stack-relevant-registers '())
-  (define reg-sources '())
-
-  (define (zenAppend array element)
-    (set! array (append array element)))
-
-  (define (regSource-handler reg-name reg-source)
-    (let ((val (assoc reg-name reg-sources)))
-      (if val
-        (append! val (list reg-source))
-        (set! reg-sources (cons (cons reg-name (list reg-source)) reg-sources)))))
-
-  (define (helper insts saved restored assigned gotoed tested branched performed)
-    (if (null? insts)
-      (begin (set! sorted-instructions (append! assigned saved restored gotoed tested branched performed))
-             (list "Instructions: " sorted-instructions 
-                   "Entry-points: " entry-points 
-                   "Stack-Relevant: " stack-relevant-registers
-                   "Register Assignement sources" reg-sources))
-      (if (symbol? (car insts))
-        ;; SKIP LABELS, THEY DONT *REALLY* AFFECT THE EXECUTION, (except they kinda d-)
-        (helper (cdr insts) saved restored assigned gotoed tested branched performed)
-        (let ((first-inst (car insts)))
-          (cond ((eq? (car first-inst) 'save)
-                 (begin (zenAppend stack-relevant-registers (list (stack-inst-reg-name first-inst)))
-                        (helper (cdr insts) (append saved (list first-inst)) restored assigned gotoed tested branched performed)))
-                ((eq? (car first-inst) 'restore)
-                 (begin (zenAppend stack-relevant-registers (list (stack-inst-reg-name first-inst)))
-                        (helper (cdr insts) saved (append restored (list first-inst)) assigned gotoed tested branched performed)))
-                ((eq? (car first-inst) 'goto)
-                 (begin (if (register-exp? (goto-dest first-inst)) (zenAppend entry-points (list (goto-reg-extract (goto-dest first-inst)))))
-                        (helper (cdr insts) saved restored assigned (append gotoed (list first-inst)) tested branched performed)))
-                ((eq? (car first-inst) 'test)
-                 (helper (cdr insts) saved restored assigned gotoed (append tested (list first-inst)) branched performed))
-                ((eq? (car first-inst) 'branch)
-                 (helper (cdr insts) saved restored assigned gotoed tested (append branched (list first-inst)) performed))
-                ((eq? (car first-inst) 'perform)
-                 (helper (cdr insts) saved restored assigned gotoed tested branched (append performed (list first-inst))))
-                ((eq? (car first-inst) 'assign)
-                 (begin (let ((target-reg (assign-reg-name first-inst))
-                              (reg-source (assign-value-exp first-inst)))
-                          (regSource-handler target-reg reg-source))
-                        (helper (cdr insts) saved restored (append assigned (list first-inst)) gotoed tested branched performed))))))))
-  (helper insts '() '() '() '() '() '() '()))
-
-
+(define (inst-scan insts)
+  (define reg-source (make-hash-table))
+  (define (insts->entry insts);{{{
+    (let* ((raw-insts (filter (lambda (arg) (not (symbol? arg))) insts))
+           (goto-insts (filter
+                         (lambda(arg-inst)
+                           (and (eq? (car arg-inst) 'goto)
+                                (eq? (caadr arg-inst) 'reg)))
+                         raw-insts)))
+      (delete-duplicates (map cadadr goto-insts))));}}}
+  (define (insts->stack-regs insts);{{{
+    (let* ((raw-insts (filter (lambda (arg) (not (symbol? arg))) insts))
+           (stack-insts (filter
+                          (lambda (arg-inst)
+                            (or (eq? (car arg-inst) 'save)
+                                (eq? (car arg-inst) 'restore)))
+                          raw-insts)))
+      (delete-duplicates (map cadr stack-insts))));}}}
+  (define (insts->reg-sources! insts);{{{
+    (hash-clear! reg-source)
+    (let* ((raw-insts (filter (lambda (arg) (not (symbol? arg))) insts))
+           (assign-insts (delete-duplicates (filter 
+                                              (lambda (arg-inst)
+                                                (eq? (car arg-inst) 'assign))
+                                              raw-insts))))
+      (for-each (lambda(arg-inst) (let ((reg (cadr arg-inst))
+                                        (source (cddr arg-inst)))
+                                    (if (not (hashq-ref reg-source reg))
+                                      (hashq-set! reg-source reg (list source))
+                                      (hashq-set! reg-source
+                                                  reg
+                                                  (cons source
+                                                        (hashq-ref reg-source
+                                                                   reg))))))
+                assign-insts)));}}}
+  (define (insts->sorted insts);{{{
+    (let* ((raw-insts (filter (lambda (arg) (not (symbol? arg))) insts)))
+      (delete-duplicates (sort raw-insts (lambda(inst inst2) (string< 
+                                                               (symbol->string (car inst))
+                                                               (symbol->string (car inst2))))))));}}}
+  (insts->reg-sources! insts)
+  (pretty-print (list "Sorted Instructions: " (insts->sorted insts)
+                      "Entry point registers: " (insts->entry insts)
+                      "Stack-interacting registers: " (insts->stack-regs insts)
+                      "Register sources: " (hash-fold
+                                             (lambda(k v p)
+                                               (cons (list k v) p))
+                                             '() reg-source))))
