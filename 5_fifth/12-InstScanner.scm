a13218451bd105631a64329998dd88183050d8cd
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-01-07 21:42:16 -0500

    Finally.
    My GOD this took me a heccing while.
    Well, that's just another hiccup along the way, hopefully.
    Note that my implementation isn't perfect at all, but at this point as long as it works.
    Gotta focus on the core of ideas, I reckon (altho there is room for improvement, but that will come later)

--- 5_fifth/12-InstScanner.scm
+++ 5_fifth/12-InstScanner.scm
@@ -1,5 +1,7 @@
 (load "ch5-regsim.scm")
 
+(use-modules (ice-9 pretty-print)) ; Pretty printing
+(use-modules (srfi srfi-1)) ; Remove duplicate
 ;; Nota bene: To make the changes required, I think I should:
 ;; 1 - Load the regular register simulator.
 ;; 2 - Override the make-machine procedure and make some modifications on it.
@@ -121,6 +123,73 @@
           ((install-entries) (lambda (arg) (set! entry-regs arg)))
           ((install-stack-regs) (lambda (arg) (set! stack-regs arg)))
           ((install-sorted) (lambda (arg) (set! sorted-instructions arg)))
+          ((show-inst-scan) (pretty-print (list "Sorted Instructions: " sorted-instructions
+                                                "Entry point registers: " entry-regs
+                                                "Stack-interacting registers: " stack-regs
+                                                "Register sources: " (hash-fold
+                                                                       (lambda(k v p)
+                                                                         (cons (list k v) p))
+                                                                       '() reg-sources))))
           (else (error "Unknown request -- MACHINE" message))));}}}
       dispatch)));}}}
+
+
+
+(define expt-machine
+  (make-machine
+    '(n b val continue)
+    (list (list '* *) (list '- -) (list '= =))
+    '(
+      (assign continue (label done))
+      loop
+      (test (op =) (reg n) (const 0))
+      (branch (label base-case))
+      (save continue)
+      (assign continue (label after))
+      (assign n (op -) (reg n) (const 1))
+      (goto (label loop))
+      after
+      (restore continue)
+      (assign val (op *) (reg val) (reg b))
+      (goto (reg continue))
+      base-case
+      (assign val (const 1))   
+      (goto (reg continue))
+      done)))
+
+(define fib-machine 
+  (make-machine ;register-names ops controller-text 
+    '(n val continue) 
+    (list (list '< <) (list '- -) (list '+ +)) 
+    '(  ; from ch5.scm 
+        (assign continue (label fib-done)) 
+        fib-loop 
+        (test (op <) (reg n) (const 2)) 
+        (branch (label immediate-answer)) 
+        ;; set up to compute Fib(n-1) 
+        (save continue) 
+        (assign continue (label afterfib-n-1)) 
+        (save n)                           ; save old value of n 
+        (assign n (op -) (reg n) (const 1)); clobber n to n-1 
+        (goto (label fib-loop))            ; perform recursive call 
+        afterfib-n-1                         ; upon return, val contains Fib(n-1) 
+        (restore n) 
+        (restore continue) 
+        ;; set up to compute Fib(n-2) 
+        (assign n (op -) (reg n) (const 2)) 
+        (save continue) 
+        (assign continue (label afterfib-n-2)) 
+        (save val)                         ; save Fib(n-1) 
+        (goto (label fib-loop)) 
+        afterfib-n-2                         ; upon return, val contains Fib(n-2) 
+        (assign n (reg val))               ; n now contains Fib(n-2) 
+        (restore val)                      ; val now contains Fib(n-1) 
+        (restore continue) 
+        (assign val                        ; Fib(n-1)+Fib(n-2) 
+                (op +) (reg val) (reg n))  
+        (goto (reg continue))              ; return to caller, answer is in val 
+        immediate-answer 
+        (assign val (reg n))               ; base case: Fib(n)=n 
+        (goto (reg continue)) 
+        fib-done)))
 
